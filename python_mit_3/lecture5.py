6d30b5b326c2528addd57fe12631c4d6b03e6545
Mauricio <mauricio004@hotmail.com>
2016-01-08 23:21:04 -0800

    update

--- python_mit_3/lecture5.py
+++ python_mit_3/lecture5.py
@@ -2,25 +2,33 @@ __author__ = 'MFlores1'
 
 import random
 
-
-def draw_balls():
-    """
-    0 = red balls
-    1 = green balls
-
-    """
-
-    # Need to remove winning ball
-    return random.choice([0, 0, 0, 1, 1, 1])
+#
+# def draw_balls(previous):
+#     """
+#     0 = red balls
+#     1 = green balls
+#     """
+#
+#     if previous != '':
+#         balls_list.remove(previous)
+#     return random.choice(balls_list)
 
 
 def no_replacement_simulation(num_trials):
     num_wins = 0.0
     for i in range(num_trials):
-        previous = 0
+        balls_list = ['b', 'b', 'b', 'g', 'g', 'g']
+        drawing_balls_lst = []
         for j in range(3):
-            d = draw_balls()
-            total = d + previous
-            previous = d
-            if total == 0 or total == 3:
-                num_wins += 1
+            d = random.choice(balls_list)
+            drawing_balls_lst.append(d)
+            # balls_list.remove(d)
+        green_count = drawing_balls_lst.count('g')
+        black_count = drawing_balls_lst.count('b')
+        # P(green) or P(black) = P(green) + P(black)
+        if green_count == 3 or black_count == 3:
+            num_wins += 1
+    r = num_wins / num_trials
+    return r
+
+print no_replacement_simulation(10000)
--- rp_conversion/test_connection.py
+++ rp_conversion/test_connection.py
@@ -36,7 +36,7 @@ def inserting_data():
         (8, 'Volkswagen', 21600))
 
     try:
-        con = psycopg2.connect(database='matching', user='postgres')
+        con = psycopg2.connect(database='conversion', user='postgres')
         cur = con.cursor()
         cur.execute("DROP TABLE IF EXISTS Cars")
         cur.execute("CREATE TABLE Cars(Id INT PRIMARY KEY, Name TEXT, Price INT)")
@@ -75,12 +75,12 @@ def retrieve_data_one_by_one():
     con = None
 
     try:
-        con = psycopg2.connect(database='matching', user='postgres')
+        con = psycopg2.connect(database='conversion', user='postgres')
         cur = con.cursor()
         cur.execute("SELECT * FROM Cars")
         while True:
             row = cur.fetchone()
-            if row == None:
+            if None:
                 break
             print row[0], row[1], row[2]
     except psycopg2.DatabaseError, e:
@@ -91,8 +91,35 @@ def retrieve_data_one_by_one():
             con.close()
 
 
+def import_one_site_residents(file_name):
+    con = None
+    f = None
+
+    try:
+        con = psycopg2.connect(database='conversion', user='postgres')
+        cur = con.cursor()
+        copy_sql = "COPY onesite_residents FROM STDIN WITH CSV HEADER DELIMITER as ','"
+
+    # finally:
+        print copy_sql
+        with open(file_name, 'r') as f:
+            cur.copy_expert(sql=copy_sql, file=f)
+            con.commit()
+    except psycopg2.DatabaseError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    except IOError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
+        if f:
+            f.close()
 
 # version()
 # inserting_data()
 # retrieve_data()
-retrieve_data_one_by_one()
+# retrieve_data_one_by_one()
+file_name = "C:/Users/mflores1/dropbox/Mauricio/matching/forest_city_residents.csv"
+import_one_site_residents(file_name)
