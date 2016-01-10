de7b9fc4708e7d7b98113e1bbbf5fd7a27a18833
Mauricio <mauricio004@hotmail.com>
2016-01-09 20:18:17 -0800

    update

--- ci/generatefeedvector.py
+++ ci/generatefeedvector.py
@@ -37,8 +37,8 @@ def getWords(html):
     return [word.lower() for word in words if word != '']
 
 
-FILENAME = "C:/Users/mflores1/Dropbox/Mauricio/ci/chapter_3/feedlist.txt"
-appcount = {}
+FILENAME = "C:/Users/mflores1/Dropbox/Mauricio/ci/chapter_3/feedlist_sample.txt"
+apcount = {}
 wordcounts = {}
 feedlist = [line for line in file(FILENAME)]
 for feedurl in feedlist:
@@ -46,11 +46,31 @@ for feedurl in feedlist:
         title, wc = getwordcounts(feedurl)
         wordcounts[title] = wc
         for word, count in wc.items():
-            appcount.setdefault(word, 0)
+            apcount.setdefault(word, 1)
             if count > 1:
-                appcount[word] += 1
+                apcount[word] += 1
 
     except:
         print 'Failed to parse feed %s' % feedurl
 
-x = 8
+wordlist = []
+for (w, bc) in apcount.items():
+    frac = float(bc) / len(feedlist)
+    # if 0.1 < frac < 0.5:
+    wordlist.append(w)
+
+FILENAME_MATRIX = "C:/Users/mflores1/Dropbox/Mauricio/ci/chapter_3/blogdata.txt"
+out = file(FILENAME_MATRIX, 'w')
+out.write('Blog')
+for word in wordlist:
+    out.write('\t%s' % word)
+out.write('\n')
+for (blog, wc) in wordcounts.items():
+    blog = blog.encode('ascii', 'ignore')
+    out.write(blog)
+    for word in wordlist:
+        if word in wc:
+            out.write('\t%d' % wc[word])
+        else:
+            out.write('\t0')
+    out.write('\n')
---
+++ rp_conversion/conversion_connection.py
@@ -0,0 +1,67 @@
+__author__ = 'MFlores1'
+
+import psycopg2
+import sys
+
+
+def import_customers_onesite(file_name):
+    con = None
+    f = None
+
+    try:
+        con = psycopg2.connect(database='conversion', user='postgres')
+        cur = con.cursor()
+        copy_sql = "COPY customers_onesite FROM STDIN WITH CSV HEADER DELIMITER as ','"
+
+    # finally:
+        print copy_sql
+        with open(file_name, 'r') as f:
+            cur.copy_expert(sql=copy_sql, file=f)
+            con.commit()
+    except psycopg2.DatabaseError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    except IOError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
+        if f:
+            f.close()
+
+
+def import_prospects_level_one(file_name):
+    con = None
+    f = None
+
+    try:
+        con = psycopg2.connect(database='conversion', user='postgres')
+        cur = con.cursor()
+        copy_sql = "COPY prospects_level_one FROM STDIN WITH CSV HEADER DELIMITER as ','"
+
+    # finally:
+        print copy_sql
+        with open(file_name, 'r') as f:
+            cur.copy_expert(sql=copy_sql, file=f)
+            con.commit()
+    except psycopg2.DatabaseError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    except IOError, e:
+        print 'Error %s' % e
+        sys.exit(1)
+    finally:
+        if con:
+            con.close()
+        if f:
+            f.close()
+
+# version()
+# inserting_data()
+# retrieve_data()
+# retrieve_data_one_by_one()
+# file_name = "C:/Users/mflores1/dropbox/Mauricio/matching/forest_city_residents.csv"
+# import_customers_onesite(file_name)
+file_name = "C:/Users/mflores1/dropbox/Mauricio/matching/l1_guest_cards.csv"
+import_prospects_level_one(file_name)
