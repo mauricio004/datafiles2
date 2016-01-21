1842617c4f05d115f9842d8d2cd4277209b77935
Mauricio <mauricio004@hotmail.com>
2016-01-20 16:00:45 -0800

    update

--- rp/avl_word_count.py
+++ rp/avl_word_count.py
@@ -1,15 +1,18 @@
 __author__ = 'MFlores1'
 
 import re
+import copy
 from avl_word_extract_2 import read_data_from_file
 from avl_word_extract_2 import read_text_for_rent
 
 
-def split_text_in_words(customer_dict):
+def split_text_in_words(customer_dict, key_dict):
     customer_wc_dict = {}
     # Loop over all customer ids
     for (key, value) in customer_dict.items():
         wc = {}
+        # Create a deep copy of keyword_dict
+        keyword_dict = copy.deepcopy(key_dict)
         # Select date and text for each customer id
         (date, text) = value
         # Extract a list of words
@@ -17,7 +20,14 @@ def split_text_in_words(customer_dict):
         for word in words:
             wc.setdefault(word, 0)
             wc[word] += 1
-        customer_wc_dict[key] = wc
+        # Compare word count with keywords
+        match = False
+        for k, v in keyword_dict.items():
+            if k in wc:
+                keyword_dict[k] += 1
+                match = True
+        customer_wc_dict[key] = (date, match, wc, keyword_dict)
+
 
 
 def get_words(txt):
@@ -42,4 +52,4 @@ filename_txt = "C:/Users/mflores1/dropbox/Mauricio/avln/for_rent_4_month.csv"
 customer_dict = read_text_for_rent(read_data_from_file(filename_txt))
 filename_keywords = "C:/Users/mflores1/dropbox/Mauricio/avln/keywords.txt"
 keyword_dict = read_keywords(filename_keywords)
-split_text_in_words(customer_dict)
+split_text_in_words(customer_dict, keyword_dict)
