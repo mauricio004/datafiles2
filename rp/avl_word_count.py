8fa967f3def5aa98ec0b6f39c2bd159a821687e8
Mauricio <mauricio004@hotmail.com>
2016-01-21 18:50:56 -0800

    update

--- rp/avl_word_count.py
+++ rp/avl_word_count.py
@@ -2,6 +2,8 @@ __author__ = 'MFlores1'
 
 import re
 import copy
+import csv
+import operator
 from avl_word_extract_2 import read_data_from_file
 from avl_word_extract_2 import read_text_for_rent
 
@@ -27,7 +29,7 @@ def split_text_in_words(customer_dict, key_dict):
                 keyword_dict[k] += 1
                 match = True
         customer_wc_dict[key] = (date, match, wc, keyword_dict)
-
+    return customer_wc_dict
 
 
 def get_words(txt):
@@ -39,6 +41,7 @@ def get_words(txt):
 
 
 def read_keywords(filename):
+    # Read keywords from list in disk
     keywords_dict = {}
     try:
         with open(filename, 'r') as f:
@@ -48,8 +51,43 @@ def read_keywords(filename):
         print 'Error reading file ' + str(err)
     return keywords_dict
 
+
+def write_result_to_file(customer_dict, filename_results):
+    # Write .csv file to disk
+    try:
+        with open(filename_results, 'wb') as f_output:
+            writer = csv.writer(f_output, delimiter=',')
+            for k, v in customer_dict.iteritems():
+                date, match_boolean, comments_count, keywords_count = v
+                # sort keywords by name (key in dictionary)
+                sorted_x = sorted(keywords_count.items(), key=operator.itemgetter(0))
+                writer.writerow([k, sorted_x[0][1], sorted_x[1][1], sorted_x[2][1], sorted_x[3][1], sorted_x[4][1],
+                                 sorted_x[5][1], sorted_x[6][1], sorted_x[7][1], sorted_x[8][1], sorted_x[9][1],
+                                 sorted_x[10][1], sorted_x[11][1], sorted_x[12][1], sorted_x[13][1], sorted_x[14][1],
+                                 sorted_x[15][1], sorted_x[16][1], sorted_x[17][1], sorted_x[18][1], sorted_x[19][1],
+                                 sorted_x[20][1], sorted_x[21][1], sorted_x[22][1], sorted_x[23][1], sorted_x[24][1],
+                                 sorted_x[25][1], sorted_x[26][1], sorted_x[27][1], sorted_x[28][1], sorted_x[29][1]])
+    except Exception as err:
+        print 'Error writing: ' + str(err)
+
+
+
+    # # Create .csv file with some base data and new field
+    # try:
+    #     with open(dir_output + 'output_s_ns.csv', 'wb') as f_output:
+    #         writer = csv.writer(f_output, delimiter=',')
+    #         for k_out, v_out in dict_base.iteritems():
+    #             writer.writerow([k_out, v_out[1], v_out[3], v_out[4], v_out[6], v_out[9], v_out[10], v_out[11],
+    #                              v_out[34]])
+    # except Exception as err:
+    #     print 'Error writing: ' + str(err)
+
+
+
 filename_txt = "C:/Users/mflores1/dropbox/Mauricio/avln/for_rent_4_month.csv"
 customer_dict = read_text_for_rent(read_data_from_file(filename_txt))
 filename_keywords = "C:/Users/mflores1/dropbox/Mauricio/avln/keywords.txt"
 keyword_dict = read_keywords(filename_keywords)
 split_text_in_words(customer_dict, keyword_dict)
+filename_result = "C:/Users/mflores1/dropbox/Mauricio/avln/keywords_result.csv"
+write_result_to_file(split_text_in_words(customer_dict, keyword_dict), filename_result)
